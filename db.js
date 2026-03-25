c31c762dcd3977c16a932fb10d471dd075dadd11
Max <ishchenkomaksym46@gmail.com>
2026-03-25 18:03:33 +0200

    Test-db 3

--- db.js
+++ db.js
@@ -2,5 +2,5 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 export const pool = new Pool({
-    connectionString: process.env.DATABSE_URL
+    connectionString: process.env.DATABASE_URL
 })
